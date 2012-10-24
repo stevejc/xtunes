5475fa956434764da64904040bca7d5c57c7e913
Steve <stevemargc@gmail.com>
2012-10-24 15:44:26 -0400

    updated artist model to include carrierwave uploader

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,9 @@
 class Artist < ActiveRecord::Base
-  attr_accessible :name, :dob, :bio
+  attr_accessible :name, :dob, :bio, :remote_photo_url
   
   has_and_belongs_to_many :songs
+  
+  mount_uploader :photo, PikUploader  
+  
 
 end
