a96eb97624c91fef306d871d82a6dcc70bcb32ca
Steve <stevemargc@gmail.com>
2012-10-25 15:07:34 -0400

    added public home page and updated route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Xtunes::Application.routes.draw do
   devise_for :users
   
   resources :songs, :albums, :artists, :genres, :mixtapes
+  
+  match '/home' => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +54,7 @@ Xtunes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'songs#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
