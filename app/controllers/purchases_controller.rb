3e62ea206917dc1383f36009ffa70f5f8cf8432e
Steve <stevemargc@gmail.com>
2012-10-26 17:48:08 -0400

    added ransack search to purchases index, now can search by name or genre just like song index page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,12 @@
 class PurchasesController < ApplicationController
   
   def index
+    @search = current_user.songs.search(params[:q])
     if current_user
-     @songs = current_user.songs
+     @songs = @search.result
     else
       @songs = []
     end
   end
 end
+
