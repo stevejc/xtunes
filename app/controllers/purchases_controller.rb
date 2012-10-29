0071d7478b2366aeb5f96ef058ca4b725f5bf1e8
Steve <stevemargc@gmail.com>
2012-10-29 16:49:39 -0400

    update search results on purchases controller so only uniq songs returned, duplicate songs were showing on purchases index as a result of songs having many genres

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,7 @@ class PurchasesController < ApplicationController
     @search = current_user.songs.search(params[:q])
     if current_user
      @songs = @search.result
+     @songs = @songs.uniq
     else
       @songs = []
     end
