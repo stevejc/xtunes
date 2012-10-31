b1735839ecb6ef5887ed74d6a949dbf668c08be0
Steve <stevemargc@gmail.com>
2012-10-31 18:00:00 -0400

    created a dashboard on the home index page so can view songs, mixtapes and add new songs right from one page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,31 @@
 class HomeController < ApplicationController
   
   def index
+    @search = Song.search(params[:q])
+    if current_user.try(:admin?) || !current_user || current_user.songs.empty?
+      @songs = @search.result
+    else
+      ids_to_exclude = []
+        current_user.songs.each do |x|
+          ids_to_exclude <<  x.id
+        end
+      songs_table = Arel::Table.new(:songs)
+      @search = Song.where(songs_table[:id].not_in ids_to_exclude).search(params[:q])
+      @songs = @search.result
+    end
+    @songs = @songs.uniq
+    
+    @mixtapes = Mixtape.find_all_by_user_id(current_user)
+    
+    @search_your_songs = current_user.songs.search(params[:q])
+    if current_user
+     @your_songs = @search_your_songs.result
+     @your_songs = @your_songs.uniq
+    else
+      @your_songs = []
+    end
+    
+    
   end
   
 end
